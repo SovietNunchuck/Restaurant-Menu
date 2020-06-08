3d38a115cac43dce06028906eae58f39d9c4b0bb
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-08 13:08:17 -0400

    First commit

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        private DateTime lastUpdated = new DateTime(2020, 6, 5);
+
+        public Menu()
+        {
+            
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+        public MenuItem(double price, string description, string category, bool isNew)
+        {
+            this.Price = price;
+            this.Description = description;
+            this.Category = category;
+            this.IsNew = isNew;
+        }
+
+    }
+}
